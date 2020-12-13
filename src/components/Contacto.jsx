8a7608b85aac567e89dee0d3280c39100590a69b
camiloberrio <camiloberriouribe@gmail.com>
2020-12-13 10:49:36 -0500

    Se ajusta pagina de contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Layout from './shared/Layout'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Layout from './shared/Layout';
 
 const Contacto = () => {
   return (
     <Layout>
+      <Link target="_blank" to="http://wa.me/+573228450982" />
     </Layout>
-  )
-}
+  );
+};
 
-export default Contacto
+export default Contacto;
