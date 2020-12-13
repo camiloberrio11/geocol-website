564220d8e8115385d36b154a5a2d7cf05e8100c7
camiloberrio <camiloberriouribe@gmail.com>
2020-12-13 12:19:12 -0500

    Se modifica ruta

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -28,9 +29,9 @@ const Header = () => {
                 </li>
 
                 <li>
-                  <a href="/empresa">
+                  <Link to="/empresa">
                     Empresa<span className="submenu-indicator"></span>
-                  </a>
+                  </Link>
                 </li>
 
                 <li>
