6175ee57828b83fa471974fb7680c72d395d7506
camiloberrio <camiloberriouribe@gmail.com>
2020-12-13 12:22:30 -0500

    Probando rutas

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -29,9 +29,9 @@ const Header = () => {
                 </li>
 
                 <li>
-                  <Link to="/empresa">
+                  <NavLink to="/empresa">
                     Empresa<span className="submenu-indicator"></span>
-                  </Link>
+                  </NavLink>
                 </li>
 
                 <li>
