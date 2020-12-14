80798f29ef5bef99d446bdc899dbf92b0015a554
camiloberrio <camiloberriouribe@gmail.com>
2020-12-13 19:04:28 -0500

    Se realiza implementacion de proyectos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/components/Proyectos.css'
 import BannerPage from './shared/BannerPage';
 import Layout from './shared/Layout';
 
@@ -7,6 +8,216 @@ const Proyectos = () => {
     <>
       <Layout>
         <BannerPage titulo="Nuestros proyectos" />
+
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col text-center">
+                <div className="sec-heading mx-auto">
+                  <h2>Un poco de nuestra experiencia</h2>
+                  <p>
+                    En los proyectos en los que hemos estado nos hemos destacado
+                    por la gran calidad a la hora de hacer las cosas, te dejamos
+                    alguno de los proyectos en los cuales hemos sido participes
+                    de su ejecución.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p className="probando">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eius ullam totam repellat saepe, est, suscipit id incidunt
+                    magni quaerat expedita facilis illum sunt eveniet nam
+                    nesciunt excepturi officiis dolor! Nulla? Ad porro explicabo
+                    libero eos exercitationem vitae ipsam eius rem error,
+                    suscipit dolorem totam, excepturi deleniti magnam.
+                    Architecto, commodi provident. Perferendis esse earum
+                    officia dolores dignissimos dolore sint nisi aliquid!
+                    Architecto laboriosam suscipit, ipsam ipsa harum eius
+                    quaerat pariatur ex sint blanditiis voluptates optio cum
+                    corrupti inventore velit accusantium soluta sed. Expedita,
+                    soluta delectus. Quibusdam molestiae saepe molestias
+                    excepturi quaerat? Nobis alias ab ipsam temporibus at nulla
+                    itaque cumque, consequatur fugit nemo modi voluptates odio
+                    quae veniam, molestias voluptatum sint consectetur nisi hic
+                    commodi voluptatibus minima. Ab fugit cum quos.
+                    Reprehenderit est deserunt inventore, voluptatum rerum
+                    provident deleniti modi illum perferendis excepturi eius
+                    soluta sit itaque molestiae alias quasi maxime dolorem quam
+                    molestias rem dolorum asperiores atque velit corrupti?
+                    Consectetur? Culpa nemo, totam, consequatur pariatur
+                    accusantium non blanditiis ad cum harum impedit officia
+                    ipsam eligendi, libero dolorem corporis assumenda voluptates
+                    quaerat alias nostrum magnam. Blanditiis a odit incidunt
+                    inventore iste. Nihil blanditiis fuga beatae iusto
+                    molestias, dolorem dolorum non possimus error natus alias
+                    quaerat sint. Alias, nemo repellendus necessitatibus
+                    delectus, nesciunt eius vitae maxime cum saepe quaerat odio
+                    libero illo. Iure atque aliquam autem necessitatibus.
+                    Voluptates vero, temporibus quam aliquid ducimus tenetur
+                    veniam voluptate officia illum impedit excepturi. Saepe quas
+                    consectetur ipsum repellendus odit dolorem dolor quasi
+                    corrupti earum aut! Repellat unde inventore recusandae
+                    repudiandae facilis quisquam id. Inventore odio ipsum harum
+                    delectus. Error sit velit nobis aliquam non, exercitationem
+                    tempore eveniet sunt laboriosam soluta, provident quam?
+                    Voluptatibus, obcaecati laboriosam. Perferendis obcaecati
+                    aliquid at veniam non, id dignissimos laudantium sit culpa,
+                    quis voluptates quidem voluptatum, earum cum eveniet tenetur
+                    molestiae quod reprehenderit tempore! Dolorum nobis
+                    repudiandae impedit tempora? Consequuntur, perspiciatis?
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p className="probando">
+                    Start working with an company that provide everything you
+                    need to any create awareness drive
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p className="probando">
+                    Start working with an company that provide everything you
+                    need to any create awareness drive
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p>
+                    Start working with an company that provide everything you
+                    need to any create awareness drive
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p>
+                    Start working with an company that provide everything you
+                    need to any create awareness drive
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-4">
+                <div
+                  className="large-features-box text-center mb-4"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                >
+                  <div className="large-features-icon-box d-table">
+                    <img
+                      src="https://via.placeholder.com/350x350"
+                      className="img-responsive"
+                      alt=""
+                    />
+                  </div>
+                  <h3>Digital Marketing</h3>
+                  <p>
+                    Start working with an company that provide everything you
+                    need to any create awareness drive
+                  </p>
+                  <a href="javascript:void(0)" className="tw-readmore">
+                    Read More
+                    <i className="ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="clearfix"></div>
       </Layout>
     </>
   );
